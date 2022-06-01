e3a570203b09716ed8539230ebb3d76fa60ce04c
AliAlShah <96441735+AliAlShah@users.noreply.github.com>
2022-06-01 12:54:10 +0300

    Migrated Database and More
    Changed main.py to app.py
    Modified database:
       1. Added instruction column
        2. Added round column
    Updated index.html form
    deleted all old database objects

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from distutils.log import debug
-from fileinput import filename
-from itertools import count
 from flask import Flask, flash, redirect, session, url_for, render_template, request, send_file
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from io import BytesIO
 import pickle
 
@@ -12,38 +10,50 @@ app.secret_key = "Im The Best"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///models.sqlite3'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class models(db.Model):
     _id = db.Column("id", db.Integer, primary_key=True)
     filename = db.Column(db.String(50))
     filedescription = db.Column(db.String(200))
     data = db.Column(db.LargeBinary)
+    instructions = db.Column(db.LargeBinary)
+    isround = db.Column(db.Boolean)
 
-    def __init__(self, filename, filedescription, data):
+    def __init__(self, filename, filedescription, data, instructions, isround):
         self.filename = filename
         self.filedescription = filedescription
         self.data = data
+        self.instructions = instructions
+        self.round = isround
 
 @app.route("/", methods=["POST", "GET"])
 def home():
     if request.method == "POST":
-        search = request.form["search"]
+        
         name = request.form["mname"]
         description = request.form["mdescription"]
         data = request.files["model"]
+        instructions = request.files["minstruct"]
+        isround = request.form["mround"]
+
+        instructionsfilename = instructions.filename
+        new_instruct = instructionsfilename.split(".")
+
         filename = data.filename
         new_data = filename.split(".")
         if new_data[1] == "pickle":
+            if new_instruct[1] == "txt":
 
-            x = models.query.filter_by(filename=name).count()
-            print(x)
-            if x > 0:
-                flash("This name has already been used!", "warning")
+                if models.query.filter_by(filename=name).count() > 0:
+                    flash("This name has already been used!", "warning")
+                else:
+                    model = models(filename=name, filedescription=description, data=data.read(), instructions=instructions.read(), isround=isround)
+                    db.session.add(model)
+                    db.session.commit()
+                    return redirect(url_for("view_model", name=name))
             else:
-                model = models(filename=name, filedescription=description, data=data.read())
-                db.session.add(model)
-                db.session.commit()
-                return redirect(url_for("view_model", name=name))
+                flash("Instructions have to be a .txt file")
         else:
             flash("Model has to be a .pickle file")
 
@@ -70,8 +80,12 @@ def view_model(name):
                 break
         x = BytesIO(mymodel.data)
         m = pickle.load(x)
-        flash(f"Prediction: {str(m.predict([parameters]))}")
-
+        prediction = m.predict([parameters])
+        if mymodel.isround == True:
+            prediction = round(prediction[0])
+        
+        flash(f"Prediction: {str(prediction)}")
+        
     return render_template("viewmodel.html", model=mymodel)
 
 @app.route("/search", methods=["POST", "GET"])

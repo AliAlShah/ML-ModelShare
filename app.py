f120add0af4a99611b7a337b5b779e2071151383
AliAlShah <96441735+AliAlShah@users.noreply.github.com>
2022-07-06 10:04:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, flash, redirect, session, url_for, render_template, request, send_file
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
 from io import BytesIO
 import difflib
 import pickle
+import os
 
 app = Flask(__name__)
+load_dotenv()
 app.config["SECRET_KEY"] = "Im the best"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hmnnapyknlmwzg:85e545ccbae5247376329fd8d4412dab30a6d9796c00230abeb488bd2bfb6529@ec2-44-198-82-71.compute-1.amazonaws.com:5432/d3lrki06atuot9'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 #app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
